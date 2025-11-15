63a8e52534213b2652c7c877c04bc0d88c141426
sohan2503001 <sohanjambhule973@gmail.com>
2025-11-15 23:35:23 +0530

    removed trailing clash

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { } from 'react'
 import {useState} from 'react'
-import {InputBox} from './components/'
-import useCurrencyInfo from './hooks/useCurrencyInfo.js'
+import {InputBox} from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
 
